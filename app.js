b22a5fd0fe62dae506aad2cd158c7c41f52167b1
wegiveyoubrand <wegiveyoubrand@gmail.com>
2021-08-30 06:03:24 +0100

    Updated

---
+++ app.js
@@ -0,0 +1,22 @@
+// call required modules
+const express = require("express");
+
+const ejs = require("ejs");
+
+// set to use
+const app = express("");
+app.use(express.static("public"));
+
+app.set("view engine", "ejs");
+
+// listen to port
+app.listen(3000);
+
+// routing
+app.get("/", function (req, res) {
+  res.render("index", { title: "Home" });
+});
+
+app.get("/dashboard", function (req, res) {
+  res.render("dashboard", { title: "Dashboard" });
+});
